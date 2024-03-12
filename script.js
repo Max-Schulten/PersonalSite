44048309533441d53cfb05eb318d14bd5186ebb2
Maximilian Schulten <maximilian.schulten1@gmail.com>
2024-03-12 11:04:53 -0400

    First functional changes, still very preliminary

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-    
+    var navItems = document.querySelectorAll('.nav-link');
+    var tabs = document.querySelectorAll('.tab-pane')
+
+    navItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+        // Remove active class from all nav-items
+        navItems.forEach(function(nav) {
+            nav.classList.remove('active');
+        });
+
+        // Add 'active' class to clicked nav-item
+        item.classList.add('active');
+
+        // Fade active tabs
+        tabs.forEach(function(tab){
+            let id = "#"+tab.id
+            if($(id).hasClass('active')){
+                $(id).removeClass('active')
+                $(id).addClass('fade')
+            }
+        });
+        // Activate selected tab
+        $($(this).attr('href')).removeClass('fade')
+        $($(this).attr('href')).addClass('active')
+        });
+    });
 })
